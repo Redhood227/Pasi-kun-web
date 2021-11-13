f0a6e367cdfe30e37ef69bfdd9dc972c4ca5613f
Redhood227 <59549868+Redhood227@users.noreply.github.com>
2021-11-14 03:02:26 +0700

    Add files via upload

--- edit_form.php
+++ edit_form.php
@@ -22,17 +22,11 @@
         <link rel="stylesheet" href="main.css">
     </head>
      <!--Line login process-->
-     <script src="https://restapi.tu.ac.th/tuapi/resources/assets/js/plugin/webfont/webfont.min.js"></script>
-    <link rel="stylesheet" href="https://restapi.tu.ac.th/tuapi/resources/assets/css/azzara.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://static.line-scdn.net/liff/edge/versions/2.5.0/sdk.js"></script>
-    <script src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+     <script src="https://static.line-scdn.net/liff/edge/versions/2.5.0/sdk.js"></script>
     <script src="https://restapi.tu.ac.th/tuapi/resources/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
     <script src="https://restapi.tu.ac.th/tuapi/resources/assets/js/core/popper.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
         <script>
             liff.init({ liffId: "1656562991-6qEqpDY4" }, () => {
