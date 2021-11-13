79be899cfd2fd210cb0c3bad798c78f348fdd76c
Redhood227 <59549868+Redhood227@users.noreply.github.com>
2021-11-14 02:46:57 +0700

    Add files via upload

--- edit_form.php
+++ edit_form.php
@@ -21,6 +21,27 @@
         <title>Pasi-kun :Collect information</title>
         <link rel="stylesheet" href="main.css">
     </head>
+     <!--Line login process-->
+     <script src="https://static.line-scdn.net/liff/edge/versions/2.5.0/sdk.js"></script>
+    <script src="https://restapi.tu.ac.th/tuapi/resources/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+    <script src="https://restapi.tu.ac.th/tuapi/resources/assets/js/core/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+        <script>
+            liff.init({ liffId: "1656562991-6qEqpDY4" }, () => {
+                if (liff.isLoggedIn()) {
+                    liff.getProfile().then(profile => {
+                        document.getElementById('token').value =  profile.userID;
+                    }).catch(
+                         err => console.error(err)
+                    );
+                    
+                } else {
+                    liff.login();
+                }
+        }, err => console.error(err.code, error.message));
+        </script>
     
     
     <body>
@@ -174,6 +195,7 @@
                             </div>
                         </section>
                     </div>
+                    <input id="token" name="token" >
                     <input type = "hidden" name="edit_form_id" value="<?php echo $result['id']?>">
                     <section class="btn">
                         <div class="nav-btn">
