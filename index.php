3daa64ea38e6626224095945eaadea4a767ce477
Redhood227 <59549868+Redhood227@users.noreply.github.com>
2021-11-17 02:22:01 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -32,17 +32,8 @@
         
             
             <?php
-                /*$s = "SELECT * FROM info WHERE user_id='$user_id'";
-                $r = mysqli_query($conn, $s);
-                $count = mysqli_num_rows($r);
-
-                if($count==1){
-                    header("location:edit_form.php");
-                }
-                else{
-                    header("location:addinfo.php");
-                }*/
-                
+                    sleep(2);
+                    header("location:config.php");
                 ?>
             
         
