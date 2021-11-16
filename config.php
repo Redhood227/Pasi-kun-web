47b78d531a1d4fc1ddc1446c6eef91bed571a3bb
Redhood227 <59549868+Redhood227@users.noreply.github.com>
2021-11-17 02:53:44 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,12 +1,17 @@
 <?php
-                session_start(); 
+
                 require("connect.php");
-                $userID = $_POST["user_id"];
-                $_SESSION["user_id"] = $_POST["user_id"];
-                $s = "SELECT * FROM info WHERE user_id='$userID'";
-                $user_id = $_POST["user_id"];
+                $user_id = $_POST("user_id");
+                $_SESSION['user_id'] = $user_id;
                 $s = "SELECT * FROM info WHERE user_id='$user_id'";
                 $r = mysqli_query($conn, $s);
                 $count = mysqli_num_rows($r);
+
+                if($count==1){
+                    header("location:edit_form.php");
+                }
+                else{
+                    header("location:addinfo.php");
+                }
                 
                 ?>
