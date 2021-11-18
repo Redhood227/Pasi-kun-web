6beb90294b25cdc293afeaf889abc796938e320d
Redhood227 <59549868+Redhood227@users.noreply.github.com>
2021-11-19 01:10:34 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <html>
     <head>
         <title>user info</title>
-        <link rel="stylesheet" href="main.css">
     </head>
     <body>
     <script src="https://static.line-scdn.net/liff/edge/versions/2.5.0/sdk.js"></script>
@@ -29,7 +28,6 @@
                     liff.login();
                 }
         }, err => console.error(err.code, error.message));
-        <div class="loader"></div>
 
         </script>
         <form id="theForm" method="POST" action="config.php">
