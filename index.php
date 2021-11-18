dbf9394759b35e4e01251ad89395d65f73a6cbe9
Redhood227 <59549868+Redhood227@users.noreply.github.com>
2021-11-19 01:23:40 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 .loader {
+    position: fixed;
+    left: 50%;
+    top: 35%;
   border: 16px solid #f3f3f3;
   border-radius: 50%;
   border-top: 16px solid #3498db;
